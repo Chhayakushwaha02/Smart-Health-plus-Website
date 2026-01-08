230efb963daa40c20a905937f7e0bb36a834a73f
Chhaya Kushwaha <chhayakushwaha0202@gmail.com>
2026-01-09 05:06:49 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,13 @@
 import sqlite3
 import os
 
-DB_PATH = os.path.join(os.path.dirname(__file__), "smarthealthplus.db")
+# Use Render persistent disk (or fallback locally)
+DB_PATH = os.environ.get("DB_PATH") or os.path.join(os.path.dirname(__file__), "smarthealthplus.db")
 
 def get_db_connection():
     try:
         conn = sqlite3.connect(DB_PATH)
-        conn.row_factory = sqlite3.Row  # So we can access columns by name
-        # Enable foreign key support
+        conn.row_factory = sqlite3.Row
         conn.execute("PRAGMA foreign_keys = ON;")
         return conn
     except sqlite3.Error as e:
